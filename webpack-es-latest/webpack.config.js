df324f9ee95d59ad741fef3aceec75a571a6c3c2
Sunny Mittal <sunnymittal2003@gmail.com>
2016-12-20 23:25:27 -0800

    Add webpack-es-latest

---
+++ webpack-es-latest/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['latest']
+      }
+    }]
+  }
+}
