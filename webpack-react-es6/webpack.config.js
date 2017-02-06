0ae0344b083a804fd1ab08c21aadef78bb8d5aff
Sunny Mittal <sunnymittal2003@gmail.com>
2017-02-05 21:53:21 -0800

    Fix webpack config for webpack 2+

--- webpack-react-es6/webpack.config.js
+++ webpack-react-es6/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
-      loader: 'babel',
+      loader: 'babel-loader',
       query: {
         presets: ['react', 'es2015']
       }
