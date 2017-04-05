9ce2b60588d0ffe84ae6a4df21393dc803ff2541
Sunny Mittal <sunnymittal2003@gmail.com>
2017-04-05 02:13:39 -0700

    Add silly sample schema fields with mongo integration

--- react-relay-graphql/index.js
+++ react-relay-graphql/index.js
@@ -1,17 +1,25 @@
 import { graphql } from 'graphql'
 import readline from 'readline'
-
+import { MongoClient } from 'mongodb'
+import assert from 'assert'
 import schema from './schema/main'
 
-const rli = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
+const MONGO_URL = 'mongodb://localhost:27017/test'
 
-rli.question('Client Request: ', query => {
-  graphql(schema, query).then(result => {
-    console.log('Server Answer: ', result.data)
+MongoClient.connect(MONGO_URL, (err, db) => {
+  assert.equal(null, err)
+  console.log('Connected to MongoDB server')
+
+  const rli = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
   })
 
-  rli.close()
+  rli.question('Client Request: ', query => {
+    graphql(schema, query).then(result => {
+      console.log('Server Answer: ', result.data)
+    })
+
+    rli.close()
+  })
 })
--- react-relay-graphql/schema/main.js
+++ react-relay-graphql/schema/main.js
@@ -1,13 +1,33 @@
 import {
   GraphQLSchema,
-  GraphQLObjectType
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLList
 } from 'graphql'
+
+const roll = () => Math.floor(6 * Math.random()) + 1
 
 const queryType = new GraphQLObjectType({
   name: 'RootQuery',
-  fields: {}
+  fields: {
+    hello: {
+      type: GraphQLString,
+      resolve: () => 'world'
+    },
+    diceRoll: {
+      type: new GraphQLList(GraphQLInt),
+      args: {
+        count: { type: GraphQLInt, defaultValue: 2 }
+      },
+      resolve: (a, args) => {
+        let rolls = []
+        for (let i = 0; i < args.count; i++) rolls.push(roll())
+        return rolls
+      }
+    }
+  }
 })
 
 const schema = new GraphQLSchema({ query: queryType })
 export default schema
-
