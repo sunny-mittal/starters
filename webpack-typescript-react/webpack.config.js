71cc9cd97c872e7797bb4113bf4c4c6d3240b1f3
Sunny Mittal <sunnymittal2003@gmail.com>
2018-04-08 18:41:11 -0700

    webpack-typescript-react

---
+++ webpack-typescript-react/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  mode: 'none',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  }
+}
